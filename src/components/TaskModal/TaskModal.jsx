a7da94c1bdfe454240d3baf49d6fb1cb087a28ac
muskaanshah <hmuskaan11@gmail.com>
2022-05-07 19:08:01 +0530

    feat: task modal

---
+++ src/components/TaskModal/TaskModal.jsx
@@ -0,0 +1,154 @@
+import React, { useState } from 'react';
+
+import { DesktopDatePicker, LocalizationProvider } from '@mui/lab';
+import {
+  Button,
+  Chip,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+} from '@mui/material';
+import { green, indigo, orange, red } from '@mui/material/colors';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+
+const TaskModal = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(new Date());
+  const [status, setStatus] = useState('');
+  const [priority, setPriority] = useState('');
+  const [progress, setProgress] = useState('');
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleStatusChange = (event) => {
+    setStatus(event.target.value);
+  };
+  const handlePriorityChange = (event) => {
+    setPriority(event.target.value);
+  };
+  const handleProgressChange = (event) => {
+    setProgress(event.target.value);
+  };
+  return (
+    <div>
+      <Button variant='outlined' onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        width='lg'
+        onClose={handleClose}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'>
+        <DialogContent sx={{ width: '100%' }}>
+          <Stack spacing={3}>
+            <TextField
+              id='standard-multiline-static'
+              label='Task name'
+              multiline
+              maxRows={3}
+              variant='standard'
+            />
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DesktopDatePicker
+                label='Due date'
+                value={value}
+                minDate={new Date('2017-01-01')}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+            <TextField
+              id='standard-multiline-static'
+              label='Description'
+              multiline
+              maxRows={5}
+              variant='standard'
+            />
+            <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id='status'>Status</InputLabel>
+              <Select labelId='status' value={status} onChange={handleStatusChange} label='Status'>
+                <MenuItem value=''>
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value='To do'>
+                  <Chip label='To do' size='small' sx={{ backgroundColor: red[300] }} />
+                </MenuItem>
+                <MenuItem value='Done'>
+                  <Chip label='Done' size='small' sx={{ backgroundColor: green[300] }} />
+                </MenuItem>
+                <MenuItem value='Doing'>
+                  <Chip label='Doing' size='small' sx={{ backgroundColor: orange[300] }} />
+                </MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id='priority'>Priority</InputLabel>
+              <Select
+                labelId='priority'
+                value={priority}
+                onChange={handlePriorityChange}
+                label='Priority'>
+                <MenuItem value=''>
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value='Low'>
+                  <Chip label='Low' size='small' sx={{ backgroundColor: green[300] }} />
+                </MenuItem>
+                <MenuItem value='Medium'>
+                  <Chip label='Medium' size='small' sx={{ backgroundColor: orange[300] }} />
+                </MenuItem>
+                <MenuItem value='High'>
+                  <Chip label='High' size='small' sx={{ backgroundColor: red[300] }} />
+                </MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
+              <InputLabel id='progress'>Progress</InputLabel>
+              <Select
+                labelId='progress'
+                value={progress}
+                onChange={handleProgressChange}
+                label='Progress'>
+                <MenuItem value=''>
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value='On track'>
+                  <Chip label='On track' size='small' sx={{ backgroundColor: green[300] }} />
+                </MenuItem>
+                <MenuItem value='Off track'>
+                  <Chip label='Off track' size='small' sx={{ backgroundColor: orange[300] }} />
+                </MenuItem>
+                <MenuItem value='Bug'>
+                  <Chip label='Bug' size='small' sx={{ backgroundColor: red[300] }} />
+                </MenuItem>
+                <MenuItem value='Need help'>
+                  <Chip label='Need help' size='small' sx={{ backgroundColor: indigo[300] }} />
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button variant='contained' onClick={handleClose} autoFocus>
+            Update
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default TaskModal;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import { Typography } from '@mui/material';
 
+import { TaskModal } from '../../components';
+
 const HomePage = () => {
   return (
     <div>
       <Typography variant='h1' component='h2' sx={{ fontFamily: 'Nova Slim, cursive' }}>
         HOMEPAGE
       </Typography>
+      <TaskModal />
     </div>
   );
 };
