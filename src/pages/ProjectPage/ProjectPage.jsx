10e30803a09280610acfe7d2af06ffc6f6048192
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-05-08 02:42:33 +0530

    fix: link fixed

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { Link, Outlet, useParams } from 'react-router-dom';
+import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 
 import { Box, Stack, Tab, Tabs } from '@mui/material';
 
 const ProjectPage = () => {
-  const [value, setValue] = useState(0);
+  const { pathname } = useLocation();
+  const path = pathname.split('/')[4];
+  const [value, setValue] = useState(path === undefined ? 0 : path === 'files' ? 1 : 2);
   const handleChange = (event, newValue) => {
     console.log(newValue);
     setValue(newValue);
@@ -16,24 +18,9 @@ const ProjectPage = () => {
     <Stack direction='column' sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} variant='fullWidth'>
-          <Tab
-            label='Tasks'
-            to={`/team/${teamId}/${projectId}`}
-            component={Link}
-            id='simple-tab-0'
-          />
-          <Tab
-            label='Files'
-            to={`/team/${teamId}/${projectId}/files`}
-            component={Link}
-            id='simple-tab-1'
-          />
-          <Tab
-            label='Dashboard'
-            to={`/team/${teamId}/${projectId}/dashboard`}
-            component={Link}
-            id='simple-tab-2'
-          />
+          <Tab label='Tasks' to={`/team/${teamId}/${projectId}`} component={Link} />
+          <Tab label='Files' to={`/team/${teamId}/${projectId}/files`} component={Link} />
+          <Tab label='Dashboard' to={`/team/${teamId}/${projectId}/dashboard`} component={Link} />
         </Tabs>
       </Box>
       <Outlet />
