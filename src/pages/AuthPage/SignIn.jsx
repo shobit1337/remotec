d9b9687a616be5b571aa0209897cdeae67f013fa
muskaanshah <hmuskaan11@gmail.com>
2022-05-08 03:52:18 +0530

    refactor: keys from env

--- src/pages/AuthPage/SignIn.jsx
+++ src/pages/AuthPage/SignIn.jsx
@@ -22,17 +22,17 @@ const SignIn = () => {
   gapi.load('client', () => {
     console.log('loaded client');
 
-    const CLIENT_ID = '976439338379-ddv0i53kracqq6dai54mlunomgen30jl.apps.googleusercontent.com';
-    const API_KEY = 'AIzaSyDgWwLJs73HaIHN25qGvKJBEIas2y0UhuE';
-    const DISCOVERY_DOCS = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
-    const SCOPES =
-      'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/calendar.events';
+    // const CLIENT_ID = '976439338379-ddv0i53kracqq6dai54mlunomgen30jl.apps.googleusercontent.com';
+    // const API_KEY = 'AIzaSyDgWwLJs73HaIHN25qGvKJBEIas2y0UhuE';
+    // const DISCOVERY_DOCS = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+    // const SCOPES =
+    //   'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid https://www.googleapis.com/auth/calendar.events';
 
     gapi.client.init({
-      apiKey: API_KEY,
-      clientId: CLIENT_ID,
-      discoveryDocs: DISCOVERY_DOCS,
-      scope: SCOPES,
+      apiKey: import.meta.env.VITE_API_KEY,
+      clientId: import.meta.env.VITE_CLIENT_ID,
+      discoveryDocs: import.meta.env.VITE_DISCOVERY_DOCS,
+      scope: import.meta.env.VITE_SCOPES,
     });
     gapi.client.load('calendar', 'v3', () => console.log('bam!'));
   });
