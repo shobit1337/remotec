04bf62a04d5580dd01017b2b132c9a9d57a2f9b5
aman <aman.dubey1998@gmail.com>
2022-05-08 01:14:59 +0530

    feat: added file display page

--- src/pages/ProjectFilesPage/ProjectFilesPage.jsx
+++ src/pages/ProjectFilesPage/ProjectFilesPage.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
 
+import BackupTableIcon from '@mui/icons-material/BackupTable';
+import FilePresentIcon from '@mui/icons-material/FilePresent';
+import { Box, Container, Paper, Stack } from '@mui/material';
+
+const flexCenter = { display: 'flex', alignItems: 'center', justifyContent: 'center' };
+
 const ProjectFilesPage = () => {
-  return <div>ProjectFilesPage</div>;
+  return (
+    <Container maxWidth={false} sx={{ ...flexCenter }}>
+      {/* <Paper variant='outlined' sx={{ ...flexCenter, width: '30rem', height: '20rem' }}>
+        <Stack
+          sx={{
+            alignItems: 'center',
+            gap: '2rem',
+            textAlign: 'center',
+          }}>
+          <BackupTableIcon fontSize='large' />
+          All attachments to tasks & messages in this project will appear here.
+        </Stack>
+      </Paper> */}
+      <Box
+        sx={{
+          justifyContent: 'space-between',
+          p: '2rem',
+          width: '100%',
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: '2rem',
+        }}>
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((e) => (
+          <Paper
+            variant='outlined'
+            sx={{
+              cursor: 'pointer',
+            }}>
+            <Box
+              sx={{
+                p: '5px',
+                justifyContent: 'space-between',
+                display: 'flex',
+              }}>
+              title
+              <FilePresentIcon />
+            </Box>
+            <img
+              src={`${'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e'}?w=248&fit=crop&auto=format`}
+            />
+          </Paper>
+        ))}
+      </Box>
+    </Container>
+  );
 };
 
 export default ProjectFilesPage;
