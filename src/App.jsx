b9537af1a44de9ef03727a3a56c84371ebfddeb4
shobit1337 <shobitdeshwal1337@gmail.com>
2022-05-07 09:22:00 +0530

    feat: eslint prettier husky pwa setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,24 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+
+import {
+  CssBaseline,
+  Typography,
+} from '@mui/material';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='App'>
+      {/* Normalizes styles */}
+      <CssBaseline />
+        <Typography variant='h1' component='h2' sx={{ fontFamily: 'Nova Slim, cursive' }}>
+          Remotic
+        </Typography>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { registerSW } from 'virtual:pwa-register';
+
+import App from './App';
+import './index.css';
+
+registerSW();
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+        <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
