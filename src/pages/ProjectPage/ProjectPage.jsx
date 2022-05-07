7d631b388a988fe3d899fd7c33c215892d73bf34
aman <aman.dubey1998@gmail.com>
2022-05-08 02:16:04 +0530

    feat: added page not found

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
-import { Box, ListItem, ListItemText, Stack } from '@mui/material';
+import { Box, Stack, Tab, Tabs } from '@mui/material';
 
 const ProjectPage = () => {
+  const [value, setValue] = useState(0);
+  const handleChange = (event, newValue) => {
+    console.log(newValue);
+    setValue(newValue);
+  };
+
+  const { projectId, teamId } = useParams();
+
   return (
     <Stack direction='column' sx={{ width: '100%' }}>
-      {/* Project Tabs */}
-      <Box sx={{ display: 'flex' }}>
-        <ListItem button sx={{ textAlign: 'center' }}>
-          <ListItemText primary='Tasks' />
-        </ListItem>
-
-        <ListItem button sx={{ textAlign: 'center' }}>
-          <ListItemText primary='Files' />
-        </ListItem>
-
-        <ListItem button sx={{ textAlign: 'center' }}>
-          <ListItemText primary='Dashboard' />
-        </ListItem>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} variant='fullWidth'>
+          <Tab
+            label='Tasks'
+            to={`/team/${teamId}/${projectId}`}
+            component={Link}
+            id='simple-tab-0'
+          />
+          <Tab
+            label='Files'
+            to={`/team/${teamId}/${projectId}/files`}
+            component={Link}
+            id='simple-tab-1'
+          />
+          <Tab
+            label='Dashboard'
+            to={`/team/${teamId}/${projectId}/dashboard`}
+            component={Link}
+            id='simple-tab-2'
+          />
+        </Tabs>
       </Box>
       <Outlet />
     </Stack>
