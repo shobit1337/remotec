3e9b3f6ff3b1fc22a9b16ef071f49965bf4e9f72
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-05-08 19:11:08 +0530

    feat: completed teams page

--- src/pages/TeamsPage/TeamsPage.jsx
+++ src/pages/TeamsPage/TeamsPage.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import AddIcon from '@mui/icons-material/Add';
@@ -9,6 +10,8 @@ import {
   Button,
   Grid,
   IconButton,
+  ListItemButton,
+  ListItemText,
   Paper,
   Stack,
   Typography,
@@ -16,14 +19,11 @@ import {
 } from '@mui/material';
 import { indigo } from '@mui/material/colors';
 
+import { ProjectModal } from '../../components';
+import { useWorkspace } from '../../context';
+import { getAllWorkspaceMemebers } from '../../utils/members';
+import { getAllProjects } from '../../utils/team';
 import { RemoveUserModal } from './components';
-
-const paperSx = {
-  width: '100%',
-  height: '100%',
-  overflow: 'auto',
-  p: 1,
-};
 
 const flexCenter = {
   display: 'flex',
@@ -31,19 +31,38 @@ const flexCenter = {
   justifyContent: 'center',
 };
 
-const memberStackSx = {
-  height: `calc(100% - 3rem)`,
-  overflowY: 'auto',
-  p: 1,
-};
-
 const TeamsPage = () => {
   const theme = useTheme();
+  const navigate = useNavigate();
+  const { workspace } = useWorkspace();
+  const [workMates, setWorkMates] = useState([]);
+  const [projects, setProjects] = useState([]);
   const [isRemoveUseModelOpen, setIsRemoveUserModelOpen] = useState(false);
-  const toggleOpen = () => setIsRemoveUserModelOpen((prev) => !prev);
+  const [isProjectModelOpen, setIsProjectModelOpen] = useState(false);
+
+  const toggleProjectModalOpen = () => setIsProjectModelOpen((state) => !state);
+  const toggleRemoveUserModalOpen = () => setIsRemoveUserModelOpen((prev) => !prev);
+
+  useEffect(() => {
+    if (workspace?.uid) {
+      (async () => {
+        const projectsRes = await getAllProjects(workspace?.uid);
+        setProjects(projectsRes);
+      })();
+      (async () => {
+        const workMatesRes = await getAllWorkspaceMemebers(workspace?.uid);
+        setWorkMates(workMatesRes);
+      })();
+    }
+  }, [workspace]);
 
   return (
     <Box>
+      <ProjectModal
+        open={isProjectModelOpen}
+        toggleOpen={toggleProjectModalOpen}
+        id={workspace?.uid}
+      />
       <Box sx={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
         <Typography variant='h5'>Team Name</Typography>
         <Button title='Copy team code'>
@@ -51,96 +70,77 @@ const TeamsPage = () => {
         </Button>
       </Box>
 
-      <Grid
-        container
-        spacing={3}
+      <Stack
         sx={{
-          height: `calc(100vh - 11rem)`,
-          marginTop: '1rem',
+          display: 'flex',
+          flexDirection: { xs: 'column', sm: 'row' },
+          gap: '1rem',
+          width: '100%',
+          margin: '1rem 0rem',
         }}>
-        <Grid item xs={12} sm={6} sx={{ height: '100%' }}>
-          <Paper
-            variant='outlined'
-            sx={{
-              ...paperSx,
-            }}>
-            <Typography variant='h6' sx={{ marginBottom: '1rem' }} gutterBottom>
-              Projects
-            </Typography>
-            <Grid
-              container
-              sx={{ height: `calc(100% - 3rem)`, overflowY: 'auto' }}
-              columnSpacing={5}
-              rowSpacing={2}>
-              <Grid
-                item
-                xs={12}
-                md={6}
+        <Paper sx={{ width: '100%', height: `calc(100vh - 13rem)` }}>
+          <Typography variant='h6' sx={{ p: '1rem' }}>
+            Projects
+          </Typography>
+          <Stack spacing='1' sx={{ p: '0 0.5rem', overflowY: 'auto', height: `calc(100% - 5rem)` }}>
+            <ListItemButton onClick={toggleProjectModalOpen}>
+              <Box
                 sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  gap: 2,
+                  ...flexCenter,
+                  borderRadius: 2,
+                  border: `2px dashed ${theme.palette.text.primary}`,
+                  minWidth: '3rem',
+                  height: '3rem',
+                  marginRight: '0.5rem',
                 }}>
-                <Box
-                  sx={{
-                    ...flexCenter,
-                    borderRadius: 2,
-                    border: `2px dashed ${theme.palette.text.primary}`,
-                    minWidth: '3rem',
-                    height: '3rem',
-                  }}>
-                  <AddIcon />
-                </Box>
-                <Typography variant='h6'>Ddsfdfd</Typography>
-              </Grid>
-              {[1, 1, 1, 1, 11, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2].map((_, index) => (
-                <Grid
-                  key={index}
-                  item
-                  xs={12}
-                  md={6}
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    gap: 2,
-                    maxHeight: 'max-content',
-                  }}>
+                <AddIcon />
+              </Box>
+              <ListItemText primary='Create Project' />
+            </ListItemButton>
+
+            {projects.map((project) => {
+              return (
+                <ListItemButton
+                  onClick={() => navigate(`/team/${workspace.uid}/${project.uid}`)}
+                  key={project.uid}>
                   <Box
                     sx={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
+                      ...flexCenter,
                       borderRadius: 2,
-                      bgcolor: 'skyblue',
+                      bgcolor: project?.themeColor,
                       minWidth: '3rem',
                       height: '3rem',
+                      marginRight: '0.5rem',
                     }}>
                     <AccountTreeIcon />
                   </Box>
-                  <Typography variant='h6'>Ddsfdfd</Typography>
-                </Grid>
-              ))}
-            </Grid>
-          </Paper>
-        </Grid>
+                  <ListItemText primary={project.name} />
+                </ListItemButton>
+              );
+            })}
+          </Stack>
+        </Paper>
 
-        <Grid item xs={12} sm={6} sx={{ height: '100%' }}>
-          <Paper
-            variant='outlined'
-            sx={{
-              ...paperSx,
-            }}>
-            <Typography variant='h6' gutterBottom>
-              Members
-            </Typography>
-            <Stack gap='1rem' sx={{ ...memberStackSx }}>
-              {[1, 1, 1, 1, 11, 1, 1, 1, 1, 1, 1, 1, 1, 2].map((_, index) => (
-                <Box
-                  key={index}
+        <Paper
+          sx={{
+            width: '100%',
+            height: `calc(100vh - 13rem)`,
+            marginBottom: { xs: '5rem', md: '0' },
+          }}>
+          <Typography variant='h6' sx={{ p: '1rem' }}>
+            Members
+          </Typography>
+          <Stack
+            alignItems='flex-start'
+            spacing='1'
+            sx={{ p: '0 0.5rem', overflowY: 'auto', height: `calc(100% - 5rem)` }}>
+            {workMates.map((mate) => {
+              return (
+                <ListItemButton
+                  key={mate.uid}
                   sx={{
-                    display: 'flex',
-                    flex: '1',
-                    gap: '1rem',
+                    width: '100%',
+                    maxHeight: '4rem',
                     '&:hover #deleteBtn': {
                       visibility: 'visible',
                     },
@@ -150,39 +150,34 @@ const TeamsPage = () => {
                       width: '3rem',
                       height: '3rem',
                       bgcolor: indigo[300],
-                      alignSelf: 'center',
+                      marginRight: '1rem',
                     }}
                     alt='Profile Photo'
-                    // src={currentUser?.photoURL}
+                    src={mate?.photoURL}
                   />
-                  <Stack
-                    flexDirection='row'
-                    alignItems='start'
-                    justifyContent='space-between'
-                    sx={{ width: '100%' }}>
-                    <Stack>
-                      <Typography variant='h6'>Member Name</Typography>
-                      <Typography
-                        variant='p'
-                        sx={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                        member.name@gmail.com
-                      </Typography>
+                  <Stack direction='row' justifyContent='space-between' sx={{ width: '100%' }}>
+                    <Stack justifyContent='center'>
+                      <ListItemText sx={{ margin: '0' }} primary={mate.name} />
+                      <Typography> {mate.email} </Typography>
                     </Stack>
                     <IconButton
                       id='deleteBtn'
                       sx={{ visibility: { md: 'hidden', xs: 'visible' } }}
-                      onClick={toggleOpen}>
+                      onClick={toggleRemoveUserModalOpen}>
                       <CloseIcon />
                     </IconButton>
 
-                    <RemoveUserModal open={isRemoveUseModelOpen} toggleOpen={toggleOpen} />
+                    <RemoveUserModal
+                      open={isRemoveUseModelOpen}
+                      toggleOpen={toggleRemoveUserModalOpen}
+                    />
                   </Stack>
-                </Box>
-              ))}
-            </Stack>
-          </Paper>
-        </Grid>
-      </Grid>
+                </ListItemButton>
+              );
+            })}
+          </Stack>
+        </Paper>
+      </Stack>
     </Box>
   );
 };
--- src/pages/TeamsPage/components/RemoveUserModal.jsx
+++ src/pages/TeamsPage/components/RemoveUserModal.jsx
@@ -12,11 +12,11 @@ const RemoveUserModal = ({ toggleOpen, open }) => {
       <DialogTitle id='alert-dialog-title'>
         Are you sure you want to remove the user from team?
       </DialogTitle>
-      <Stack direction='row' gap='1rem' sx={{ margin: '0 1rem 1rem 1rem' }}>
+      <Stack direction='row-reverse' gap='1rem' sx={{ margin: '0 1rem 1rem 1rem' }}>
         <Button variant='contained' onClick={toggleOpen}>
           Cancel
         </Button>
-        <Button variant='contained' color='error'>
+        <Button variant='outlined' color='error'>
           Remove
         </Button>
       </Stack>
